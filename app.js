393c1596af1515799129a58664947540a767fc43
anthonycc <anthonyccardozo@gmail.com>
2021-08-16 15:06:06 -0400

    testing app.js-database connection

--- app.js
+++ app.js
@@ -2,4 +2,44 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const db = require('../u-develop-it/db/connection');
+
+
+// Connect to database
+mysql.createConnection( {
+    host: 'localhost',
+    user: 'root',
+    password: '$uperLazy123',
+    database: 'EmployeeTracker'
+    },
+    console.log('Connected to EmployeeTracker database')
+);
+
+db.connect(err => {
+    if (err) throw (err);
+    console.log('-------------------------------------')
+    console.log('|         EMPLOYEE TRACKER         |')
+    console.log('------------------------------------')
+    options();
+})
+
+// First inquirer prompts
+const options = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'Select what you would like to do.',
+            choices: ['View Departments', 
+                      'View Roles', 
+                      'View Employees', 
+                      'Add a Depertment', 
+                      'Add a Role', 
+                      'Add an Employee',
+                      'Update an Employee Role'
+                    ]
+        }
+    ])
+};
+
 
